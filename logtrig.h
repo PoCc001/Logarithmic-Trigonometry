0380f653046b34127a8a9df13e2e763ddc81981e
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2021-08-21 17:08:24 +0200

    add functions with different log bases

--- logtrig.h
+++ logtrig.h
@@ -17,18 +17,42 @@
 extern CPP_SAFE float lsinf(float);
 extern CPP_SAFE double lsin(double);
 extern CPP_SAFE long double lsinl(long double);
+extern CPP_SAFE float l10sinf(float);
+extern CPP_SAFE double l10sin(double);
+extern CPP_SAFE long double l10sinl(long double);
+extern CPP_SAFE float l2sinf(float);
+extern CPP_SAFE double l2sin(double);
+extern CPP_SAFE long double l2sinl(long double);
 
 extern CPP_SAFE float lcosf(float);
 extern CPP_SAFE double lcos(double);
 extern CPP_SAFE long double lcosl(long double);
+extern CPP_SAFE float l10cosf(float);
+extern CPP_SAFE double l10cos(double);
+extern CPP_SAFE long double l10cosl(long double);
+extern CPP_SAFE float l2cosf(float);
+extern CPP_SAFE double l2cos(double);
+extern CPP_SAFE long double l2cosl(long double);
 
 extern CPP_SAFE float ltanf(float);
 extern CPP_SAFE double ltan(double);
 extern CPP_SAFE long double ltanl(long double);
+extern CPP_SAFE float l10tanf(float);
+extern CPP_SAFE double l10tan(double);
+extern CPP_SAFE long double l10tanl(long double);
+extern CPP_SAFE float l2tanf(float);
+extern CPP_SAFE double l2tan(double);
+extern CPP_SAFE long double l2tanl(long double);
 
 extern CPP_SAFE float lcotf(float);
 extern CPP_SAFE double lcot(double);
 extern CPP_SAFE long double lcotl(long double);
+extern CPP_SAFE float l10cotf(float);
+extern CPP_SAFE double l10cot(double);
+extern CPP_SAFE long double l10cotl(long double);
+extern CPP_SAFE float l2cotf(float);
+extern CPP_SAFE double l2cot(double);
+extern CPP_SAFE long double l2cotl(long double);
 
 #ifdef __cplusplus
 #include "logtrig.hpp" // use overloaded functions without suffix
